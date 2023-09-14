04df755e00e04bd4e16b170b6fc23ae77ec4ac70
Kazakov Michael <kazakov_my@nlmk.com>
2023-09-14 14:34:59 +0300

    fix ts err

--- src/shared/utils/myDash/render.ts
+++ src/shared/utils/myDash/render.ts
@@ -1,8 +1,8 @@
 import Block from '../../classComponents/Block';
 
 export function render(query: string | undefined, block: Block): void {
-    console.log(query,'query');
-    const root = document.getElementById( 'root');
+    console.log(query, 'query');
+    const root = document.getElementById('root');
     if (root) {
         root.innerHTML = '';
         // eslint-disable-next-line new-cap
