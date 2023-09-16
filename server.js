ad7880edbdd5071a8677f9fc25148bdf711b7b95
Kazakov Michael <kazakov_my@nlmk.com>
2023-09-16 13:34:47 +0300

    fix review errors

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('./dist'));
+app.use(express.static('./dist/'));
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../dist/index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(`${__dirname}/dist/index.html`);
 });
 
 app.listen(PORT, () => {
